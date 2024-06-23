0e9ec8d01bf4c46a4fb0a5923b458a6a9db950a8
Adil Hanney <adilh+github@orchid.anonaddy.com>
2024-06-23 20:29:43 +0100

    feat: export SyncerComponent

--- lib/abstract_sync.dart
+++ lib/abstract_sync.dart
@@ -1,5 +1,6 @@
 export 'src/abstract_sync_interface.dart';
 export 'src/sync_file.dart';
+export 'src/syncer_component.dart';
 export 'src/syncer_downloader.dart';
 export 'src/syncer_uploader.dart';
 export 'src/syncer.dart';
